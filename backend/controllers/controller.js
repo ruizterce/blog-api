8d04e1828629f4c1bf04551aace76ee168a1e2a4
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-11-01 12:59:10 +0100

    feat: enhance posts fetch with author and comments
    - Modified postsGet method to include author
    username and comment count for each post in the
    fetched data

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -11,8 +11,31 @@ module.exports = {
   // Get all posts
   postsGet: async (req, res) => {
     try {
-      const posts = await prisma.post.findMany();
-      res.json(posts);
+      const posts = await prisma.post.findMany({
+        include: {
+          author: {
+            select: {
+              username: true,
+            },
+          },
+          comments: true,
+        },
+      });
+
+      // Map through posts to add the count of comments
+      const formattedPosts = posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        image: post.image,
+        text: post.text,
+        status: post.status,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+        author: post.author.username,
+        commentCount: post.comments.length,
+      }));
+
+      res.json(formattedPosts);
     } catch (error) {
       console.error("Error fetching posts:", error);
       res.status(500).json({ error: "Failed to fetch posts" });
