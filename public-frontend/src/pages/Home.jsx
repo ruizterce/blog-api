95f2a21885a8a6791d479a1d5775ce8400168f73
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-11-05 13:20:04 +0100

    feat: add search bar to Home page
    - Implemented search bar in Home page to filter
    posts dynamically
    - Posts now update in real-time based on search
    input

--- public-frontend/src/pages/Home.jsx
+++ public-frontend/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import { fetchPosts } from "../services/api";
 import PostCard from "../components/PostCard";
-import { Container, Typography, Grid2, CircularProgress } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Grid2,
+  CircularProgress,
+  Box,
+  TextField,
+} from "@mui/material";
 
 import "../styles/styles.css";
 
@@ -9,6 +16,7 @@ const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const getPosts = async () => {
@@ -24,16 +32,51 @@ const Home = () => {
     getPosts();
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPosts = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase()) || // Search in title
+      post.text.toLowerCase().includes(searchTerm.toLowerCase()) // Search in text
+  );
+
   if (loading) return <CircularProgress />;
   if (error) return <Typography color="error">Error: {error}</Typography>;
 
   return (
-    <Container className="container">
-      <Typography variant="h4" component="h1" gutterBottom>
-        Blog Posts
-      </Typography>
+    <Container className="container" sx={{ marginTop: 0 }}>
+      <Box display={"flex"} margin={1}>
+        <Typography
+          variant="h6"
+          component="h1"
+          gutterBottom
+          sx={{ flexGrow: 1, minWidth: 220, margin: 0 }}
+        >
+          Blog Posts
+        </Typography>
+        <Box sx={{ width: "100%", maxWidth: 300, flexShrink: 1 }}>
+          <TextField
+            fullWidth
+            size="small"
+            variant="outlined"
+            placeholder="Search articles..."
+            onChange={handleSearchChange}
+            sx={{
+              "& .MuiInputBase-input": {
+                fontSize: "0.8rem", // Smaller font size
+                padding: "6px 8px", // Reduced padding
+              },
+              "& .MuiOutlinedInput-root": {
+                height: "32px", // Set a custom height if needed
+              },
+            }}
+          />
+        </Box>
+      </Box>
       <Grid2 container spacing={2}>
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
           <Grid2 size={{ xs: 12, md: 6 }} key={post.id}>
             <PostCard post={post} />
           </Grid2>
