89c4dd48d9eabe389e46a7d447cb24a9f1d09c0a
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-11-08 12:41:36 +0100

    feat: add filters and sorting to public frontend
    - Added dropdown filters for author in Home
    component, dynamically populated from unique
    authors in posts data
    - Implemented ascending/descending sorting for
    post IDs with a toggleable TableSortLabel
    - Updated UI for filtering options

--- public-frontend/src/pages/Home.jsx
+++ public-frontend/src/pages/Home.jsx
@@ -8,6 +8,12 @@ import {
   CircularProgress,
   Box,
   TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Button,
+  TableSortLabel,
 } from "@mui/material";
 
 import "../styles/styles.css";
@@ -17,6 +23,8 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [authorFilter, setAuthorFilter] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
 
   useEffect(() => {
     const getPosts = async () => {
@@ -32,50 +40,67 @@ const Home = () => {
     getPosts();
   }, []);
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const uniqueAuthors = [...new Set(posts.map((post) => post.author))];
+
+  const handleSearchChange = (event) => setSearchTerm(event.target.value);
+  const handleAuthorChange = (event) => setAuthorFilter(event.target.value);
+
+  const filteredPosts = posts
+    .filter(
+      (post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post.text.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .filter((post) => (authorFilter ? post.author === authorFilter : true))
+    .sort((a, b) => {
+      if (sortOrder === "asc") return a.id - b.id;
+      return b.id - a.id;
+    });
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
   };
-
-  const filteredPosts = posts.filter(
-    (post) =>
-      post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      post.text.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   if (loading) return <CircularProgress />;
   if (error) return <Typography color="error">Error: {error}</Typography>;
 
   return (
-    <Container className="container" sx={{ marginTop: 0 }}>
-      <Box display={"flex"} marginTop={1} marginBottom={1}>
-        <Typography
-          variant="h6"
-          component="h1"
-          color="primary"
-          gutterBottom
-          sx={{ flexGrow: 1, minWidth: 220, margin: 0 }}
-        >
-          Blog Posts
-        </Typography>
-        <Box sx={{ width: "100%", maxWidth: 300, flexShrink: 1 }}>
+    <Container className="container">
+      <Box display="flex" justifyContent={"space-between"} gap={1} mb={2}>
+        <FormControl sx={{ width: 200 }} size="small">
+          <InputLabel>Filter by Author</InputLabel>
+          <Select
+            value={authorFilter}
+            onChange={handleAuthorChange}
+            label="Filter by Author"
+            size="small"
+          >
+            <MenuItem value="">All Authors</MenuItem>
+            {uniqueAuthors.map((author) => (
+              <MenuItem key={author} value={author}>
+                {author}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <Box sx={{ width: "100%", maxWidth: "60%", flexGrow: 1 }}>
           <TextField
             fullWidth
             size="small"
             variant="outlined"
             placeholder="Search articles..."
             onChange={handleSearchChange}
-            sx={{
-              "& .MuiInputBase-input": {
-                fontSize: "0.8rem",
-                padding: "6px 8px",
-              },
-              "& .MuiOutlinedInput-root": {
-                height: "32px",
-              },
-            }}
           />
         </Box>
+
+        <Button
+          onClick={toggleSortOrder}
+          sx={{ paddingLeft: 3, paddingRight: 2 }}
+        >
+          Sort <TableSortLabel active direction={sortOrder}></TableSortLabel>
+        </Button>
       </Box>
+
       {filteredPosts.length === 0 ? (
         <Typography variant="body1" color="textSecondary" sx={{ mt: 2 }}>
           No posts found matching your search criteria.
